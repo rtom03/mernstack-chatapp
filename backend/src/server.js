ba60f5e2400c492960bd5f53a6eb25a5dfff1d2e
rtom03 <zuckm709@gmail.com>
2025-03-13 21:05:11 +0100

    implement user profile

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,15 +13,15 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.use("/api/auth", authRoutes);
-app.use("/api", messageRoute);
 app.use(
   cors({
-    origin: "http://localhost:5137",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoute);
 
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
