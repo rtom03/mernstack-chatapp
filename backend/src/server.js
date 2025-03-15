181c1535dc031811eef60e92b89ed486d5913bbb
rtom03 <zuckm709@gmail.com>
2025-03-15 22:47:19 +0100

    code adjustment

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://backend-three-sandy.vercel.app"],
     credentials: true,
   })
 );
