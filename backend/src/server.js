ed467fbf3ff1993695328a04061e2633e916716f
rtom03 <zuckm709@gmail.com>
2025-03-15 23:03:10 +0100

    code adjustment

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://backend-three-sandy.vercel.app"],
+    origin: ["http://localhost:5173", "https://chimeln.vercel.app"],
     credentials: true,
   })
 );
